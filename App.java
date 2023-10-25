ddd95ac6a9956264431403fc1ece7adfdd69651a
JVDS <71608479+JVDS2@users.noreply.github.com>
2023-10-25 18:16:35 -0300

    Textos nas variáveis

--- App.java
+++ App.java
@@ -4,35 +4,44 @@ public class App {
 
     public static void main(String[] args) {
         
+        String cap1_texto = "Você está em uma floresta desconhecida. Você vê uma entrada de caverna à esquerda, e um caminho que continua na floresta pra direita.";
+        String cap1_escolha1 = "seguir para a caverna";
+        String cap1_escolha2 = "seguir pela floresta";
+
+        String cap2_texto = "Você segue pela floresta e encontra um rio. Você pode atravessar nadando, ou seguir pela margem do rio.";
+        String cap2_escolha1 = "nadar";
+        String cap2_escolha2 = "seguir pela margem";
+        
         Scanner scanner = new Scanner(System.in);
 
         boolean mainLoop = true;
 
         while(mainLoop){
 
-            System.out.println("Você está em uma floresta desconhecida. Você vê uma entrada de caverna à esquerda, e um caminho que continua na floresta pra direita.");
-            System.out.println("1. Seguir para a Caverna.\n" +
-                               "2. Seguir pela floresta.");
+            System.out.println(cap1_texto);
+            System.out.println(cap1_escolha1);
+            System.out.println(cap1_escolha2);
+
             String choice = scanner.nextLine();
 
-            if(choice.equals("1")){
+            if(choice.equals(cap1_escolha1)){
                 System.out.println("Escolha 1 - Seguir para a caverna:");
                 System.out.println("Você entra na caverna e é atacado por um urso. Você morreu!");
                 mainLoop = false;
             }
-            else if(choice.equals("2")){
+            else if(choice.equals(cap1_escolha2)){
                 while(true){
-                    System.out.println("Você segue pela floresta e encontra um rio. Você pode atravessar nadando, ou seguir pela margem do rio.");
-                    System.out.println("1. Atravessar nadando.\n" +
-                                    "2. Seguir pela margem do rio.");
+                   System.out.println(cap2_texto);
+                   System.out.println(cap2_escolha1);
+                   System.out.println(cap2_escolha2);
                     
-                    String newChoice = scanner.nextLine();
-                    if(newChoice.equals("1")){
+                    choice = scanner.nextLine();
+                    if(choice.equals(cap2_escolha1)){
                         System.out.println("Você tenta nadar pelo rio, mas é arrastado pela correnteza. Você morreu!");
                         mainLoop = false;
                         break;
                     }
-                    else if(newChoice.equals("2")){
+                    else if(choice.equals(cap2_escolha2)){
                         System.out.println("Você segue pelas margens do rio e encontra uma ponte que leva até uma pequena civilização. Fim.");
                         mainLoop = false;
                         break;
